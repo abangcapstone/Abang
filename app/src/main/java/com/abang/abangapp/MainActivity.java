d5121e332cb7f7a8d5144341392d78a90ef68064
abangcapstone <infozojt@gmail.com>
2018-01-09 05:34:35 +0800

    Ross anisdfsdfsf

--- app/src/main/java/com/abang/abangapp/MainActivity.java
+++ app/src/main/java/com/abang/abangapp/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        //Usab nissssssfffffffffffffffffffffffffffffffffff
+        //Usab nissssssfffffffasdasdasdffffffffffffffffffffffffffff    asdasd
 
     }
 }
